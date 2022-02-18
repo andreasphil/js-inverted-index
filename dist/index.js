de16754bf5176cc945450d5e20805dd767176c42
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2022-02-18 12:34:52 +0100

    1.1.0

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,18 @@
-function unwrap1(obj, prop) {
-    if (!obj) {
-        return undefined;
-    }
+// deno-fmt-ignore-file
+// deno-lint-ignore-file
+// This code was bundled using `deno bundle` and it's not recommended to edit it manually
+
+function unwrap(obj, prop) {
+    if (!obj) return undefined;
     const path = Array.isArray(prop) ? prop : prop.split(".");
     const [head, ...tail] = path;
-    if (tail.length) {
-        return unwrap1(obj[head], tail);
-    } else {
-        return obj[head];
-    }
+    if (tail.length) return unwrap(obj[head], tail);
+    else return obj[head];
 }
-function intersect1(...sets) {
+function intersect(...sets) {
     if (!sets.length || sets.some((set)=>!set
-    )) {
-        return new Set();
-    } else if (sets.length === 1) {
-        return sets[0];
-    }
+    )) return new Set();
+    else if (sets.length === 1) return sets[0];
     const setsCopy = [
         ...sets
     ];
@@ -24,20 +20,18 @@ function intersect1(...sets) {
     const b = setsCopy.shift();
     const intersection = new Set();
     a.forEach((itemFromA)=>{
-        if (b.has(itemFromA)) {
-            intersection.add(itemFromA);
-        }
+        if (b.has(itemFromA)) intersection.add(itemFromA);
     });
     setsCopy.unshift(intersection);
-    return intersect1(...setsCopy);
+    return intersect(...setsCopy);
 }
-function idProp1(prop) {
-    return (document)=>document[prop]
+function idProp(prop) {
+    return (document)=>document[prop]?.toString?.()
     ;
 }
-const lowercaseTrim1 = (input)=>input?.trim().toLowerCase()
+const lowercaseTrim = (input)=>input?.trim().toLowerCase()
 ;
-const matchAllTerms1 = (index, term, options)=>{
+const matchAllTerms = (index, term, options)=>{
     if (!term || Object.keys(index).length === 0) {
         return new Set();
     }
@@ -46,56 +40,76 @@ const matchAllTerms1 = (index, term, options)=>{
     );
     const matches = termTokens.map((token)=>index[token]
     );
-    return intersect1(...matches);
+    return intersect(...matches);
 };
-function regexSplit1(exp) {
+function regexSplit(exp) {
     return (input)=>input ? input.match(exp) || [] : []
     ;
 }
-const fullWordSplit1 = regexSplit1(/\w+/g);
-export { unwrap1 as unwrap };
-export { intersect1 as intersect };
-export { idProp1 as idProp };
-export { lowercaseTrim1 as lowercaseTrim };
-export { matchAllTerms1 as matchAllTerms };
-export { regexSplit1 as regexSplit };
-export { fullWordSplit1 as fullWordSplit };
-function addToIndex(index, documents, options) {
-    const { tokenizer , identifier , normalizer , fields  } = options;
-    return documents.reduce((newIndex, document)=>{
-        const id = identifier(document);
-        fields.map((path)=>unwrap1(document, path)
-        ).filter((value)=>!!value
-        ).flatMap((value)=>tokenizer(value.toString())
-        ).map((token)=>normalizer(token)
-        ).forEach((token)=>{
-            if (newIndex[token]) {
-                newIndex[token].add(id);
-            } else {
-                newIndex[token] = new Set([
-                    id
-                ]);
-            }
-        });
-        return newIndex;
-    }, index);
-}
-function initSearch(options = {
-}, initial) {
+const fullWordSplit = regexSplit(/\w+/g);
+export { unwrap as unwrap };
+export { intersect as intersect };
+export { idProp as idProp };
+export { lowercaseTrim as lowercaseTrim };
+export { matchAllTerms as matchAllTerms };
+export { regexSplit as regexSplit };
+export { fullWordSplit as fullWordSplit };
+function createSearch(options = {}) {
     const effectiveOptions = {
-        tokenizer: fullWordSplit1,
-        identifier: idProp1("id"),
-        normalizer: lowercaseTrim1,
+        tokenizer: fullWordSplit,
+        identifier: idProp("id"),
+        normalizer: lowercaseTrim,
+        searcher: matchAllTerms,
         fields: [],
-        searcher: matchAllTerms1,
         ...options
     };
-    const index = initial || {
+    let index = {};
+    let indexedDocuments = {};
+    const search = (term)=>{
+        const matches = [];
+        const idMatches = effectiveOptions.searcher(index, term, effectiveOptions);
+        idMatches.forEach((id)=>{
+            if (indexedDocuments[id]) matches.push(indexedDocuments[id]);
+        });
+        return matches;
+    };
+    const add = (documents)=>{
+        const { tokenizer , identifier , normalizer , fields  } = effectiveOptions;
+        documents.forEach((document)=>{
+            const id = identifier(document);
+            indexedDocuments[id] = document;
+            fields.map((path)=>unwrap(document, path)
+            ).filter((value)=>!!value?.toString
+            ).flatMap((value)=>tokenizer(value.toString())
+            ).map((token)=>normalizer(token)
+            ).forEach((token)=>{
+                if (index[token]) index[token].add(id);
+                else index[token] = new Set([
+                    id
+                ]);
+            });
+        });
+    };
+    const dump1 = ()=>Object.entries(index).reduce((all, [k, v])=>{
+            all[k] = Array.from(v);
+            return all;
+        }, {})
+    ;
+    const hydrate = (dump, documents)=>{
+        index = Object.entries(dump).reduce((all, [k, v])=>{
+            all[k] = new Set(v);
+            return all;
+        }, {});
+        indexedDocuments = documents.reduce((all, i)=>{
+            all[effectiveOptions.identifier(i)] = i;
+            return all;
+        }, {});
     };
     return {
-        add: (documents)=>addToIndex(index, documents, effectiveOptions)
-        ,
-        search: (term)=>effectiveOptions.searcher(index, term, effectiveOptions)
+        search,
+        add,
+        dump: dump1,
+        hydrate
     };
 }
-export { initSearch as default };
+export { createSearch as default };
